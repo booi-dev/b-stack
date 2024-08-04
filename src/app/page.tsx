849dfd2748cd028824baf5a94f2379008efe9ab3
booi-dev <booimangang@gmail.com>
2024-08-04 18:18:54 +0530

    add: stack list on front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,36 @@
 import Link from "next/link";
 
+const STACK_LIST = [
+  {
+    id: "sdfsf",
+    name: "Next JS",
+    role: "Full Stack Framework",
+    description:
+      "Next.js is a React framework that enables server-side rendering and static site generation for React applications.",
+  },
+  {
+    id: "sfsdfdsf",
+    name: "Next Auth",
+    role: "Authentication",
+    description:
+      "NextAuth.js is a complete open source authentication solution for Next.js applications.",
+  },
+  {
+    id: "hsghdf",
+    name: "Drizzle",
+    role: "ORM",
+    description:
+      "Drizzle is a TypeScript ORM that enables you to interact with your database using TypeScript.",
+  },
+  {
+    id: "587sdfsf",
+    name: "Tailwind CSS",
+    role: "Styling",
+    description:
+      "Tailwind CSS is a utility-first CSS framework that enables you to build custom designs without writing customCSS.",
+  },
+];
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -9,6 +40,16 @@ export default function Home() {
         <Link href="/auth" className="mt-8">
           Go To Auth Page
         </Link>
+
+        <div className="mt-4">
+          <h2 className="text-2xl text-teal-500">Stacks</h2>
+          {STACK_LIST.map((stack) => (
+            <div key={stack.id} className="flex items-center gap-4">
+              <div className="font-bold">{stack.role}</div>{" "}
+              <h3>{stack.name}</h3>
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   );
